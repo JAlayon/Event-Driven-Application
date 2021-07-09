42429dddb222f12d1c8d48460dc3c675732d4c40
Jair Alayon <jair.atl.95@gmail.com>
2021-07-09 08:57:44 -0500

    Creating the first microservice: Twitter-to-kafka-service

---
+++ twitter-to-kafka-service/src/main/java/com/alayon/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,32 @@
+package com.alayon.microservices.demo.twitter.to.kafka.service;
+
+import com.alayon.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    public TwitterToKafkaServiceApplication(final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(final String... args) throws Exception {
+        log.info("App starts...");
+        log.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[0])));
+        log.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+    }
+}
