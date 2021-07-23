f3d5f16f6899ab22fd68fe061287eaed524dd28d
Jair Alayon <jair.atl.95@gmail.com>
2021-07-23 15:00:11 -0500

    Creating docker image for config-server

--- config-server/src/main/java/com/alayon/microservices/demo/config/server/config/SecurityConfig.java
+++ config-server/src/main/java/com/alayon/microservices/demo/config/server/config/SecurityConfig.java
@@ -10,6 +10,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(final WebSecurity web) throws Exception {
         web.ignoring()
+                .antMatchers("/actuator/**")
                 .antMatchers("/encrypt/**")
                 .antMatchers("/decrypt/**");
         super.configure(web);
