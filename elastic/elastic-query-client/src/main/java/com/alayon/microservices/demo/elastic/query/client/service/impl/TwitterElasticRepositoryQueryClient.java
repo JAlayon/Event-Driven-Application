c6582f36ea1bb25d5bbfc12e4db5ee40ba039e77
Jair Alayon <jair.atl.95@gmail.com>
2021-07-27 12:57:57 -0500

    Adding classes to use elastic data repository for querying

---
+++ elastic/elastic-query-client/src/main/java/com/alayon/microservices/demo/elastic/query/client/service/impl/TwitterElasticRepositoryQueryClient.java
@@ -0,0 +1,55 @@
+package com.alayon.microservices.demo.elastic.query.client.service.impl;
+
+import com.alayon.microservices.demo.elastic.model.index.impl.TwitterIndexModel;
+import com.alayon.microservices.demo.elastic.query.client.exception.ElasticQueryClientException;
+import com.alayon.microservices.demo.elastic.query.client.repository.TwitterElasticsearchQueryRepository;
+import com.alayon.microservices.demo.elastic.query.client.service.ElasticQueryClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Primary
+@Service
+public class TwitterElasticRepositoryQueryClient implements ElasticQueryClient<TwitterIndexModel> {
+
+    private static final Logger log = LoggerFactory.getLogger(TwitterElasticRepositoryQueryClient.class);
+
+    private final TwitterElasticsearchQueryRepository repository;
+
+    public TwitterElasticRepositoryQueryClient(final TwitterElasticsearchQueryRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public TwitterIndexModel getIndexModelById(final String id) {
+        var searchResult = repository.findById(id);
+
+        searchResult.ifPresentOrElse(model -> {
+            log.info("Document with id {} retrieved successfully",model.getId());
+        }, () -> {
+            throw new ElasticQueryClientException("No document found at elasticsearch with id: " + id);
+        });
+        return searchResult.get();
+    }
+
+    @Override
+    public List<TwitterIndexModel> getIndexModelByText(final String text) {
+        var searchResult = repository.findByText(text);
+        log.info("{} of documents with text {} retrieved successfully", searchResult.size(),text);
+        return searchResult;
+    }
+
+    @Override
+    public List<TwitterIndexModel> getAllIndexModels() {
+        var iterable = repository.findAll();
+        var searchResult = StreamSupport.stream(iterable.spliterator(), false)
+                .collect(Collectors.toList());
+        log.info("{} number of documents retrieved successfully", searchResult.size());
+        return searchResult;
+    }
+}
