b0d330ebaf80f475f0747008008b412e80f4a1cc
Jair Alayon <jair.atl.95@gmail.com>
2021-10-03 14:05:02 -0500

    Changing some config. files to make work kafka-to-elastic service

--- docker-compose/check-kafka-topics-created.sh
+++ docker-compose/check-kafka-topics-created.sh
@@ -12,6 +12,7 @@ echo "kafkacat result: " $kafkacatResult
 while [[ ! $kafkacatResult == *"twitter-topic"* ]]; do
   >&2 echo "Kafka topic has not been created yet!"
   sleep 2
+  kafkacatResult=${kafkacat -L -b kafka-broker-1:9092}
 done
 
 ./cnb/process/web
