5e2e716741316708a4bedea47dbd801d992cfc53
Jair Alayon <jair.atl.95@gmail.com>
2021-10-04 11:53:55 -0500

    Adding version by media-type

--- elastic-query-service/src/main/java/com/alayon/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/alayon/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -3,6 +3,7 @@ package com.alayon.microservices.demo.elastic.query.service.api;
 import com.alayon.microservices.demo.elastic.query.service.business.ElasticQueryService;
 import com.alayon.microservices.demo.elastic.query.service.model.ElasticQueryServiceRequestModel;
 import com.alayon.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import com.alayon.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModelV2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,7 @@ import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/documents")
+@RequestMapping(value = "/documents", produces = "application/vnd.api.v1+json")
 public class ElasticDocumentController {
 
     private static final Logger log = LoggerFactory.getLogger(ElasticDocumentController.class);
@@ -39,6 +40,13 @@ public class ElasticDocumentController {
         log.info("Elasticsearch returned document with id {}", id);
         return ResponseEntity.ok(response);
     }
+    @GetMapping(value = "/{id}", produces = "application/vnd.api.v2+json")
+    public ResponseEntity<ElasticQueryServiceResponseModelV2>
+    getDocumentByIdV2(@PathVariable @NotEmpty String id){
+        var response = elasticQueryService.getDocumentById(id);
+        log.info("Elasticsearch returned document with id {}", id);
+        return ResponseEntity.ok(getV2Model(response));
+    }
 
     @GetMapping("/get-document-by-text")
     public ResponseEntity<List<ElasticQueryServiceResponseModel>>
@@ -48,5 +56,17 @@ public class ElasticDocumentController {
         var response = elasticQueryService.getDocumentByText(request.getText());
         log.info("Elasticsearch returned {} of documents", response.size());
         return ResponseEntity.ok(response);
+    }
+
+    private ElasticQueryServiceResponseModelV2 getV2Model(ElasticQueryServiceResponseModel responseModel){
+        var responseModelV2 = ElasticQueryServiceResponseModelV2
+                .builder()
+                .id(Long.parseLong(responseModel.getId()))
+                .userId(responseModel.getUserId())
+                .text(responseModel.getText())
+                .text2("Version 2 text")
+                .build();
+        responseModelV2.add(responseModel.getLinks());
+        return responseModelV2;
     }
 }
