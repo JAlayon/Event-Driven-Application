148370513cd9554e967f4bed43ed51c7c76ca43d
Jair Alayon <jair.atl.95@gmail.com>
2021-10-04 11:09:31 -0500

    Adding Hateoas capability to Rest api

--- elastic-query-service/src/main/java/com/alayon/microservices/demo/elastic/query/service/business/TwitterElasticQueryService.java
+++ elastic-query-service/src/main/java/com/alayon/microservices/demo/elastic/query/service/business/TwitterElasticQueryService.java
@@ -3,7 +3,7 @@ package com.alayon.microservices.demo.elastic.query.service.business;
 import com.alayon.microservices.demo.elastic.model.index.impl.TwitterIndexModel;
 import com.alayon.microservices.demo.elastic.query.client.service.ElasticQueryClient;
 import com.alayon.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModel;
-import com.alayon.microservices.demo.elastic.query.service.transformer.ElasticToResponseModelTransformer;
+import com.alayon.microservices.demo.elastic.query.service.model.assembler.ElasticQueryServiceResponseModelAssembler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -15,31 +15,31 @@ class TwitterElasticQueryService implements ElasticQueryService{
 
     private static final Logger log = LoggerFactory.getLogger(TwitterElasticQueryService.class);
 
-    private final ElasticToResponseModelTransformer transformer;
+    private final ElasticQueryServiceResponseModelAssembler assembler;
 
     private final ElasticQueryClient<TwitterIndexModel> elasticQueryClient;
 
-    public TwitterElasticQueryService(final ElasticToResponseModelTransformer transformer,
+    public TwitterElasticQueryService(final ElasticQueryServiceResponseModelAssembler assembler,
                                       final ElasticQueryClient<TwitterIndexModel> elasticQueryClient) {
-        this.transformer = transformer;
+        this.assembler = assembler;
         this.elasticQueryClient = elasticQueryClient;
     }
 
     @Override
     public ElasticQueryServiceResponseModel getDocumentById(final String id) {
         log.info("Querying elasticsearch by id: {}", id);
-        return transformer.getResponseModel(elasticQueryClient.getIndexModelById(id));
+        return assembler.toModel(elasticQueryClient.getIndexModelById(id));
     }
 
     @Override
     public List<ElasticQueryServiceResponseModel> getDocumentByText(final String text) {
         log.info("Querying elasticsearch by text: {}", text);
-        return transformer.getResponseModels(elasticQueryClient.getIndexModelByText(text));
+        return assembler.toModels(elasticQueryClient.getIndexModelByText(text));
     }
 
     @Override
     public List<ElasticQueryServiceResponseModel> getAllDocuments() {
         log.info("Querying all documents in elasticsearch");
-        return transformer.getResponseModels(elasticQueryClient.getAllIndexModels());
+        return assembler.toModels(elasticQueryClient.getAllIndexModels());
     }
 }
